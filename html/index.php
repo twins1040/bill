3755bfd9c2cc3c193eb5c185f7ccfac437a9883e
kkCheon <nocha1040@toss.im>
2016-12-29 20:03:26 +0900

    dd

--- html/index.php
+++ html/index.php
@@ -53,7 +53,7 @@ if (mysql_num_rows($result) == 0) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
-    <title>명욱 수정</title>
+    <title>on going</title>
 
     <!-- Toss CSS Framework CDN -->
     <link href="https://cdn.rawgit.com/tossapp/tossapp.github.io/master/framework-bt/assets/stylesheets/tossframe-latest.css" rel="stylesheet">    
